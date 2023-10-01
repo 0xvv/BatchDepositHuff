68708495304286f841150184063c0696acb91d91
0xvv <public_double_v@protonmail.com>
2023-10-01 20:07:40 +0200

    test: matching test for both contract

--- test/BatchDepositHuff.t.sol
+++ test/BatchDepositHuff.t.sol
@@ -2,50 +2,61 @@ pragma solidity 0.8.20;
 
 import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
 import "forge-std/Test.sol";
-import "./mock//DepositContractTestable.sol";
-import "./mock/DepositContractNoop.sol";
+import "./mock/DepositContractTestable.sol";
 import "./utils/BytesGenerator.sol";
 import "../src/lib/LibBytes.sol";
 
-interface IBatchDepositBis {
+interface IBatchDepositCompact {
     function deposit(bytes calldata data) external payable;
 }
 
+interface IBatchDepositClassic {
+    function deposit(
+        bytes calldata pubkeys,
+        bytes calldata withdrawal_credentials,
+        bytes calldata signatures,
+        bytes32[] calldata deposit_data_roots
+    ) external payable;
+}
+
 contract BatchDepositHuffTest is Test, BytesGenerator {
-    IBatchDepositBis huffbdo;
+    IBatchDepositCompact huffCompact;
+    IBatchDepositClassic huffClassic;
 
     address constant officialDepositContract = 0x00000000219ab540356cBB839Cbe05303d7705Fa;
 
-    DepositContractTestable huffDepMock = DepositContractTestable(officialDepositContract);
-    DepositContractNoop implem = new DepositContractNoop();
+    DepositContractTestable huffDepMock;
 
-    function deployHuff() public {
-        vm.etch(officialDepositContract, address(implem).code);
+    function deployHuffCompact() public {
         address addr = HuffDeployer.deploy("BatchDepositCompact");
         console.log("Huff deployed", addr, addr.code.length);
         assertTrue(addr != address(0));
-        huffbdo = IBatchDepositBis(addr);
+        huffCompact = IBatchDepositCompact(addr);
+    }
+
+    function deployHuffClassic() public {
+        address addr = HuffDeployer.deploy("BatchDeposit");
+        console.log("Huff classic deployed", addr, addr.code.length);
+        assertTrue(addr != address(0));
+        huffClassic = IBatchDepositClassic(addr);
     }
 
     function setUp() public {
-        deployHuff();
+        huffDepMock = new DepositContractTestable();
+        vm.etch(officialDepositContract, address(huffDepMock).code);
+
+        deployHuffCompact();
+        deployHuffClassic();
     }
 
     function checkDepositCount(uint256 count) internal {
-        assertEq(huffDepMock.deposit_count(), count);
+        assertEq(DepositContractTestable(officialDepositContract).deposit_count(), count);
     }
 
-    function test_d() public {
-        console.log("d", address(this).balance);
-    }
-
-    function test_batchDeposit_match() public {
-        //setSalt(bytes32(abi.encodePacked()));
-        //c = bound(c, 1, 200);
-        uint256 c = 1;
+    function test_batchDepositCompact_match(uint256 c) public {
+        setSalt(bytes32(abi.encodePacked(c)));
+        c = bound(c, 1, 200);
         uint256 COUNT = uint256(c);
-
-        console.log("COUNT", COUNT);
 
         bytes memory pubkeys = genBytes(48 * COUNT);
         bytes memory withdrawal_credentials = genBytes(32 * COUNT);
@@ -68,17 +79,54 @@ contract BatchDepositHuffTest is Test, BytesGenerator {
             args = bytes.concat(args, deposit_data_roots[i]);
         }
 
-        console2.log(args.length);        vm.deal(address(this), 32 ether * COUNT);
+        vm.deal(address(this), 32 ether * COUNT);
 
-        // HUFF
-        huffbdo.deposit{value: 32 ether * COUNT}(args);
-/*
-        for (uint256 i; i < COUNT; i++) {
-            assertEq(huffDepMock.depositDataRoots(i), deposit_data_roots[i]);
-            assertEq(huffDepMock.depositPubkeys(i), pubkeysList[i]);
-            assertEq(huffDepMock.depositWithdrawalCredentials(i), withdrawalCredentialsList[i]);
-            assertEq(huffDepMock.depositSignatures(i), signaturesList[i]);
+        for(uint256 i = 0; i < COUNT; i++) {
+            vm.expectEmit(true, true, true, true);
+            emit DepositEvent(pubkeysList[i], withdrawalCredentialsList[i], signaturesList[i], deposit_data_roots[i]);
         }
-        checkDepositCount(COUNT);*/
+
+        huffCompact.deposit{value: 32 ether * COUNT}(args);
+        
+        checkDepositCount(COUNT);
     }
+
+    function test_batchDepositClasssic_match(uint256 c) public {
+        setSalt(bytes32(abi.encodePacked(c)));
+        c = bound(c, 1, 200);
+        uint256 COUNT = c;
+
+        bytes memory pubkeys = genBytes(48 * COUNT);
+        bytes memory withdrawal_credentials = genBytes(32 * COUNT);
+        bytes memory signatures = genBytes(96 * COUNT);
+        bytes32[] memory deposit_data_roots = new bytes32[](COUNT);
+        bytes[] memory pubkeysList = new bytes[](COUNT);
+        bytes[] memory withdrawalCredentialsList = new bytes[](COUNT);
+        bytes[] memory signaturesList = new bytes[](COUNT);
+        for (uint256 i = 0; i < COUNT; i++) {
+            pubkeysList[i] = LibBytes.slice(pubkeys, i * 48, 48);
+            withdrawalCredentialsList[i] = LibBytes.slice(withdrawal_credentials, i * 32, 32);
+            signaturesList[i] = LibBytes.slice(signatures, i * 96, 96);
+            deposit_data_roots[i] = bytes32(genBytes(32));
+        }
+
+        vm.deal(address(this), 32 ether * COUNT);
+
+        for(uint256 i = 0; i < COUNT; i++) {
+            vm.expectEmit(true, true, true, true);
+            emit DepositEvent(pubkeysList[i], withdrawalCredentialsList[i], signaturesList[i], deposit_data_roots[i]);
+        }
+
+        huffClassic.deposit{value: 32 ether * COUNT}(pubkeys, withdrawal_credentials, signatures, deposit_data_roots);
+        
+        checkDepositCount(COUNT);
+    }
+
+
+    event DepositEvent(
+        bytes pubkey,
+        bytes withdrawal_credentials,
+        bytes signature,
+        bytes32 deposit_data_root
+    );
 }
--- test/mock/DepositContractTestable.sol
+++ test/mock/DepositContractTestable.sol
@@ -13,50 +13,20 @@ pragma solidity >=0.8.17;
 import "../../src/interface/IDeposit.sol";
 
 contract DepositContractTestable is IDeposit {
-    uint256 constant DEPOSIT_CONTRACT_TREE_DEPTH = 32;
-    // NOTE: this also ensures `deposit_count` will fit into 64-bits
-    uint256 constant MAX_DEPOSIT_COUNT = 2 ** DEPOSIT_CONTRACT_TREE_DEPTH - 1;
 
-    bytes32[DEPOSIT_CONTRACT_TREE_DEPTH] branch;
     uint256 public deposit_count;
-
-    bytes32[DEPOSIT_CONTRACT_TREE_DEPTH] zero_hashes;
 
     bytes32[] public depositDataRoots;
     bytes[] public depositPubkeys;
     bytes[] public depositWithdrawalCredentials;
     bytes[] public depositSignatures;
 
-    constructor() {
-        // Compute hashes in empty sparse Merkle tree
-        for (uint256 height = 0; height < DEPOSIT_CONTRACT_TREE_DEPTH - 1; height++) {
-            zero_hashes[height + 1] = sha256(abi.encodePacked(zero_hashes[height], zero_hashes[height]));
-        }
-    }
-
-    function get_deposit_root() external view returns (bytes32) {
-        bytes32 node;
-        uint256 size = deposit_count;
-        for (uint256 height = 0; height < DEPOSIT_CONTRACT_TREE_DEPTH; height++) {
-            if ((size & 1) == 1) {
-                node = sha256(abi.encodePacked(branch[height], node));
-            } else {
-                node = sha256(abi.encodePacked(node, zero_hashes[height]));
-            }
-            size /= 2;
-        }
-        return sha256(abi.encodePacked(node, to_little_endian_64(uint64(deposit_count)), bytes24(0)));
-    }
-
-    function get_deposit_count() external view returns (bytes memory) {
-        return to_little_endian_64(uint64(deposit_count));
-    }
-
-    bool dataRootCheck = false;
-
-    function debug_enableDataRootCheck() external {
-        dataRootCheck = true;
-    }
+    event DepositEvent(
+        bytes pubkey,
+        bytes withdrawal_credentials,
+        bytes signature,
+        bytes32 deposit_data_root
+    );
 
     function deposit(
         bytes calldata pubkey,
@@ -64,26 +34,13 @@ contract DepositContractTestable is IDeposit {
         bytes calldata signature,
         bytes32 deposit_data_root
     ) external payable override {
+        deposit_count += 1;
+        emit DepositEvent(pubkey, withdrawal_credentials, signature, deposit_data_root);
+        /*
         depositDataRoots.push(deposit_data_root);
         depositPubkeys.push(pubkey);
         depositWithdrawalCredentials.push(withdrawal_credentials);
         depositSignatures.push(signature);
-
-        // Add deposit data root to Merkle tree (update a single `branch` node)
-        deposit_count += 1;
-    }
-
-    function to_little_endian_64(uint64 value) internal pure returns (bytes memory ret) {
-        ret = new bytes(8);
-        bytes8 bytesValue = bytes8(value);
-        // Byteswapping during copying to bytes.
-        ret[0] = bytesValue[7];
-        ret[1] = bytesValue[6];
-        ret[2] = bytesValue[5];
-        ret[3] = bytesValue[4];
-        ret[4] = bytesValue[3];
-        ret[5] = bytesValue[2];
-        ret[6] = bytesValue[1];
-        ret[7] = bytesValue[0];
+        */
     }
 }
